0b04d3ab2761924b818760883c3744be1ea35e2b
bia.stransky <bia.stransky@gmail.com>
2020-07-09 19:58:27 -0300

    Trying to 'clean' the code, removing duplicate analysis (set as comment)

--- analysis/2.correlation.R
+++ analysis/2.correlation.R
@@ -13,9 +13,9 @@
 #'   rmarkdown::render(inputFile, encoding = encoding, output_format = "all") })     
 #' ---
 #' 
-#' This project contains a pipeline of clinical analysis of the Cancer Genome Atlas Kidney Renal Clear Cell Carcinoma (TCGA-KIRC) data of patients from [Genomic Data Commons Data Portal](https://portal.gdc.cancer.gov/exploration?filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases.project.project_id%22%2C%22value%22%3A%5B%22TCGA-KIRC%22%5D%7D%7D%5D%7D) and [cBioPortal](https://www.cbioportal.org/study/summary?id=kirp_tcga).
+#' This project contains a pipeline of clinical analysis of the Cancer Genome Atlas Kidney Renal Clear Cell Carcinoma (TCGA-KIRC) data of patients, from [Genomic Data Commons Data Portal](https://portal.gdc.cancer.gov/exploration?filters=%7B%22op%22%3A%22and%22%2C%22content%22%3A%5B%7B%22op%22%3A%22in%22%2C%22content%22%3A%7B%22field%22%3A%22cases.project.project_id%22%2C%22value%22%3A%5B%22TCGA-KIRC%22%5D%7D%7D%5D%7D) and [cBioPortal](https://www.cbioportal.org/study/summary?id=kirp_tcga).
 #' 
-#' Previously, we presented [an exploratory preprocessing analysis](1.preprocessing.md). In this section, we present a correlation analysis with t-test and ANOVA test to investigate significative distinctions between clinical variables according to their vital status. 
+#' Previously, we presented [an exploratory preprocessing analysis](1.preprocessing.md). In this section, Chi-squared test is applied to compare two or more proportions of categorical variables and T-student test to compare the means of numeric ones regardind the levels of 'Overall_Survival_Status'. The Hypoteis test is performed and p-value indicates the strength of evidence in supportting the null hypothesis.
 #' 
 #' 
 
@@ -23,7 +23,7 @@
 #' 
 ## ----message=FALSE, warning=FALSE, paged.print=FALSE--------------------------
 # Set the packages of interest
-packages = c("tidyverse","skimr","finalfit","rstatix","ggpubr","GGally")
+packages = c("tidyverse","skimr","finalfit","rstatix", "ggpubr","GGally")
 
 # if a package is installed, it will be loaded
 # otherwise, the missing package(s) will be installed and loaded
@@ -38,13 +38,10 @@ suppressMessages(library("tidyverse"))
 setwd(".")
 
 #' 
-#' 
 #' ## 1. Importing data
 #' 
 ## ----message=FALSE, warning=FALSE, paged.print=FALSE--------------------------
-
 kirc_clinic <- read_csv("data/kirc_clinic.csv")
-
 
 #' 
 #' ## 2. Taming data 
@@ -55,32 +52,21 @@ kirc_clinic <- kirc_clinic %>%
   mutate(patient_id = as.character(patient_id))
 
 #' 
-#' ## 3. Checking categorical variables
-#' 
-#' check frequency, lables and levels 
-#' 
-## ----echo=FALSE---------------------------------------------------------------
-
-kirc_clinic %>%
-  select_if(is.factor) %>%
-  summary() 
-
-#' 
-#' ## 4. Checking variables
+#' ## 3. Checking variables
 #' 
 ## -----------------------------------------------------------------------------
-glimpse(kirc_clinic)
 skim(kirc_clinic) 
-#View(kirc_clinic)
+# glimpse(kirc_clinic)
+# View(kirc_clinic)
 
 #' 
-#' ## 5. Numeric variables vs. over_surv_stt
+#' ## 4. Numeric variables vs. over_surv_stt
 #' graphic visualization and t-test
 #' 
-#' 
 ## ----message=FALSE, warning=FALSE---------------------------------------------
-
-cols_numeric <- kirc_clinic %>% select_if(is.numeric) %>% names
+cols_numeric <- kirc_clinic %>% 
+  select_if(is.numeric) %>%
+  names
 
 kirc_clinic_numeric <- kirc_clinic %>%
                       select(one_of(c(cols_numeric, "over_surv_stt")))  
@@ -98,7 +84,7 @@ ggpairs(kirc_clinic_numeric, columns = cols_numeric,
         
 
 #' 
-#' ### 5.1 Run multiple T-tests on over_surv_stt
+#' ### 4.1 Run multiple T-tests on over_surv_stt
 #' 
 #' Transform the data into long format
 #' 
@@ -132,7 +118,6 @@ stat.test
 
 #' 
 #' 
-#' 
 ## -----------------------------------------------------------------------------
 # Create the plot on logscale
 myplot <- ggboxplot(
@@ -162,12 +147,9 @@ graphs <- kirc_clinic_numeric.long %>%
   )
 graphs
 
-
-
 #' 
 #' 
 ## -----------------------------------------------------------------------------
-
 # Add statitistical tests to each corresponding plot
 variables <- graphs$variables
 for(i in 1:length(variables)){
@@ -177,13 +159,9 @@ for(i in 1:length(variables)){
   print(graph.i)
 }
 
-
-
-
 #' 
 #' 
 ## -----------------------------------------------------------------------------
-
 ggplot(kirc_clinic, aes(age, fill= over_surv_stt)) +
   geom_histogram(bins = 15, position = "dodge")
 t.test(kirc_clinic$age ~ kirc_clinic$over_surv_stt) 
@@ -227,113 +205,117 @@ ggplot(kirc_clinic, aes(x=over_surv_stt, y=second_long_dim)) +
   geom_jitter(width = 0.05, alpha = 0.2, color = "orange")
 t.test(kirc_clinic$second_long_dim ~ kirc_clinic$over_surv_stt)
 
-# fazer uma table com as variaveis dependentes, indpendente e p-valores
-
 #' 
-#' ## 4. Categorical variables vs. over_surv_stt
+#' ## 5. Categorical variables vs. over_surv_stt
+#' 
+#' ## 5.1 Checking categorical variables
+#' 
+## ----echo=FALSE---------------------------------------------------------------
+# TO DO: levels with < 5 observations -> hemoglobin and tumor_lateral
+kirc_clinic %>%
+  select_if(is.factor) %>%
+  summary()
+
 #' 
 #' Tabulation and chi-square test
 #' 
 ## ----warning=FALSE------------------------------------------------------------
-# talvez isso possa sair uma vez que ja tem a mesma analise com tablefit
-kirc_clinic %>%
-  select_if(is.factor) %>%
-  summary() 
-
-t_metas_stg <- table(kirc_clinic$metastasis_stg, kirc_clinic$over_surv_stt, exclude = NULL)
-t_metas_stg <- addmargins(round(100*prop.table(t_metas_stg)))
-t_metas_stg
-chisq.test(x = kirc_clinic$metastasis_stg, y = kirc_clinic$over_surv_stt) 
-
-t_lynph <- table(kirc_clinic$neoplasm_ln_stg, kirc_clinic$over_surv_stt, exclude = NULL)
-t_lynph <- addmargins(round(100*prop.table(t_lynph)))
-t_lynph
-chisq.test(x = kirc_clinic$neoplasm_ln_stg, y = kirc_clinic$over_surv_stt) 
-
-t_neop <- table(kirc_clinic$neoplasm_stg, kirc_clinic$over_surv_stt, exclude = NULL)
-t_neop <- addmargins(round(100*prop.table(t_neop)))
-t_neop
-chisq.test(x = kirc_clinic$neoplasm_stg, y = kirc_clinic$over_surv_stt) 
-
-t_tumor <- table(kirc_clinic$tumor_stg, kirc_clinic$over_surv_stt, exclude = NULL)
-t_tumor <- addmargins(round(100*prop.table(t_tumor)))
-t_tumor
-chisq.test(x = kirc_clinic$tumor_stg, y = kirc_clinic$over_surv_stt) 
-
-t_free <- table(kirc_clinic$disease_free_stt, kirc_clinic$over_surv_stt, exclude = NULL)
-t_free <- addmargins(round(100*prop.table(t_free)))
-t_free
-chisq.test(x = kirc_clinic$disease_free_stt, y = kirc_clinic$over_surv_stt) 
-
-t_prior <- table(kirc_clinic$prior_cancer, kirc_clinic$over_surv_stt, exclude = NULL)
-t_prior <- addmargins(round(100*prop.table(t_prior)))
-t_prior
-chisq.test(x = kirc_clinic$prior_cancer, y = kirc_clinic$over_surv_stt) 
-
-t_neo <- table(kirc_clinic$neoadj_therapy, kirc_clinic$over_surv_stt, exclude = NULL)
-t_neo <- addmargins(round(100*prop.table(t_neo)))
-t_neo
-chisq.test(x = kirc_clinic$neoadj_therapy, y = kirc_clinic$over_surv_stt) 
-
-t_platelet <- table(kirc_clinic$platelet, kirc_clinic$over_surv_stt, exclude = NULL)
-t_platelet <- addmargins(round(100*prop.table(t_platelet)))
-t_platelet
-chisq.test(x = kirc_clinic$platelet, y = kirc_clinic$over_surv_stt)
-
-t_prospect <- table(kirc_clinic$tissue_prospect, kirc_clinic$over_surv_stt, exclude = NULL)
-t_prospect <- addmargins(round(100*prop.table(t_prospect)))
-t_prospect
-chisq.test(x = kirc_clinic$tissue_prospect, y = kirc_clinic$over_surv_stt)
-
-t_race <- table(kirc_clinic$race, kirc_clinic$over_surv_stt, exclude = NULL)
-t_race <- addmargins(round(100*prop.table(t_race)))
-t_race
-chisq.test(x = kirc_clinic$race, y = kirc_clinic$over_surv_stt) 
-
-t_retros <- table(kirc_clinic$tissue_retrospect, kirc_clinic$over_surv_stt, exclude = NULL)
-t_retros <- addmargins(round(100*prop.table(t_retros)))
-t_retros
-chisq.test(x = kirc_clinic$tissue_retrospect, y = kirc_clinic$over_surv_stt)
-
-t_ca <- table(kirc_clinic$serum_ca, kirc_clinic$over_surv_stt, exclude = NULL)
-t_ca <- addmargins(round(100*prop.table(t_ca)))
-t_ca
-chisq.test(x = kirc_clinic$serum_ca, y = kirc_clinic$over_surv_stt) 
-
-t_gender <- table(kirc_clinic$gender, kirc_clinic$over_surv_stt, exclude = NULL)
-t_gender <- addmargins(round(100*prop.table(t_gender)))
-t_gender
-chisq.test(x = kirc_clinic$gender, y = kirc_clinic$over_surv_stt) 
-
-t_site <- table(kirc_clinic$tissue_site, kirc_clinic$over_surv_stt, exclude = NULL)
-t_site <- addmargins(round(100*prop.table(t_site)))
-t_site
-chisq.test(x = kirc_clinic$tissue_site, y = kirc_clinic$over_surv_stt) 
-
-t_neop_st <- table(kirc_clinic$person_neoplasm_stt, kirc_clinic$over_surv_stt, exclude = NULL)
-t_neop_st <- addmargins(round(100*prop.table(t_neop_st)))
-t_neop_st
-chisq.test(x = kirc_clinic$person_neoplasm_stt, y = kirc_clinic$over_surv_stt) 
-
-t_wbc <- table(kirc_clinic$wbc, kirc_clinic$over_surv_stt, exclude = NULL)
-t_wbc <- addmargins(round(100*prop.table(t_wbc)))
-t_wbc
-chisq.test(x = kirc_clinic$wbc, y = kirc_clinic$over_surv_stt) 
+# # talvez isso possa sair uma vez que ja tem a mesma analise com tablefit
+# 
+# t_metas_stg <- table(kirc_clinic$metastasis_stg, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_metas_stg <- addmargins(round(100*prop.table(t_metas_stg)))
+# t_metas_stg
+# chisq.test(x = kirc_clinic$metastasis_stg, y = kirc_clinic$over_surv_stt) 
+# 
+# t_lynph <- table(kirc_clinic$neoplasm_ln_stg, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_lynph <- addmargins(round(100*prop.table(t_lynph)))
+# t_lynph
+# chisq.test(x = kirc_clinic$neoplasm_ln_stg, y = kirc_clinic$over_surv_stt) 
+# 
+# t_neop <- table(kirc_clinic$neoplasm_stg, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_neop <- addmargins(round(100*prop.table(t_neop)))
+# t_neop
+# chisq.test(x = kirc_clinic$neoplasm_stg, y = kirc_clinic$over_surv_stt) 
+# 
+# t_tumor <- table(kirc_clinic$tumor_stg, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_tumor <- addmargins(round(100*prop.table(t_tumor)))
+# t_tumor
+# chisq.test(x = kirc_clinic$tumor_stg, y = kirc_clinic$over_surv_stt) 
+# 
+# t_free <- table(kirc_clinic$disease_free_stt, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_free <- addmargins(round(100*prop.table(t_free)))
+# t_free
+# chisq.test(x = kirc_clinic$disease_free_stt, y = kirc_clinic$over_surv_stt) 
+# 
+# t_prior <- table(kirc_clinic$prior_cancer, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_prior <- addmargins(round(100*prop.table(t_prior)))
+# t_prior
+# chisq.test(x = kirc_clinic$prior_cancer, y = kirc_clinic$over_surv_stt) 
+# 
+# t_neo <- table(kirc_clinic$neoadj_therapy, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_neo <- addmargins(round(100*prop.table(t_neo)))
+# t_neo
+# chisq.test(x = kirc_clinic$neoadj_therapy, y = kirc_clinic$over_surv_stt) 
+# 
+# t_platelet <- table(kirc_clinic$platelet, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_platelet <- addmargins(round(100*prop.table(t_platelet)))
+# t_platelet
+# chisq.test(x = kirc_clinic$platelet, y = kirc_clinic$over_surv_stt)
+# 
+# t_prospect <- table(kirc_clinic$tissue_prospect, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_prospect <- addmargins(round(100*prop.table(t_prospect)))
+# t_prospect
+# chisq.test(x = kirc_clinic$tissue_prospect, y = kirc_clinic$over_surv_stt)
+# 
+# t_race <- table(kirc_clinic$race, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_race <- addmargins(round(100*prop.table(t_race)))
+# t_race
+# chisq.test(x = kirc_clinic$race, y = kirc_clinic$over_surv_stt) 
+# 
+# t_retros <- table(kirc_clinic$tissue_retrospect, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_retros <- addmargins(round(100*prop.table(t_retros)))
+# t_retros
+# chisq.test(x = kirc_clinic$tissue_retrospect, y = kirc_clinic$over_surv_stt)
+# 
+# t_ca <- table(kirc_clinic$serum_ca, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_ca <- addmargins(round(100*prop.table(t_ca)))
+# t_ca
+# chisq.test(x = kirc_clinic$serum_ca, y = kirc_clinic$over_surv_stt) 
+# 
+# t_gender <- table(kirc_clinic$gender, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_gender <- addmargins(round(100*prop.table(t_gender)))
+# t_gender
+# chisq.test(x = kirc_clinic$gender, y = kirc_clinic$over_surv_stt) 
+# 
+# t_site <- table(kirc_clinic$tissue_site, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_site <- addmargins(round(100*prop.table(t_site)))
+# t_site
+# chisq.test(x = kirc_clinic$tissue_site, y = kirc_clinic$over_surv_stt) 
+# 
+# t_neop_st <- table(kirc_clinic$person_neoplasm_stt, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_neop_st <- addmargins(round(100*prop.table(t_neop_st)))
+# t_neop_st
+# chisq.test(x = kirc_clinic$person_neoplasm_stt, y = kirc_clinic$over_surv_stt) 
+# 
+# t_wbc <- table(kirc_clinic$wbc, kirc_clinic$over_surv_stt, exclude = NULL)
+# t_wbc <- addmargins(round(100*prop.table(t_wbc)))
+# t_wbc
+# chisq.test(x = kirc_clinic$wbc, y = kirc_clinic$over_surv_stt) 
 
 #' 
 #' ## 7. FinalFit
 #' 
-#' summarise variables/factors by a categorical variable
+#' Summarise variables/factors by a categorical variable
 #' 
 ## -----------------------------------------------------------------------------
 #  warning=FALSE
-explanatory <- names(kirc_clinic %>%
+explanatory_char <- names(kirc_clinic %>%
               select(-over_surv_stt) %>%
               select_if(is.factor))
 dependent <-  'over_surv_stt'
 
 table_fit <- kirc_clinic %>%
-  summary_factorlist(dependent, explanatory, p=TRUE, add_dependent_label=TRUE,  na_include = TRUE)
+  summary_factorlist(dependent, explanatory_char, p=TRUE, add_dependent_label=TRUE,  na_include = TRUE)
 
 
 knitr::kable(table_fit, row.names=FALSE, align=c("l", "l", "r", "r", "r"))
@@ -341,15 +323,14 @@ knitr::kable(table_fit, row.names=FALSE, align=c("l", "l", "r", "r", "r"))
 #' 
 #' Summarise numerical variables by a categorical variable
 #' 
-#' 
 ## -----------------------------------------------------------------------------
 
-explanatory <- cols_numeric
+explanatory_num <- cols_numeric
 
 dependent <-  'over_surv_stt'
 
 table_fit <- kirc_clinic %>%
-  summary_factorlist(dependent, explanatory, p=TRUE, add_dependent_label=TRUE,  na_include = TRUE, na_include_dependent = TRUE)
+  summary_factorlist(dependent, explanatory_num, p=TRUE, add_dependent_label=TRUE,  na_include = TRUE, na_include_dependent = TRUE)
 
 knitr::kable(table_fit, row.names=FALSE, align=c("l", "l", "r", "r", "r"))
 
